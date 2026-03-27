ff19cab7fb7fd666ec815d49f47bb653ddf192bc
rayene <rayenemejri74@gmail.com>
2026-03-27 18:33:03 +0100

    init repo

---
+++ src/main/java/tn/esprit/ms_auditConformite_ContratNumerique/MsAuditConformiteContratNumeriqueApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.ms_auditConformite_ContratNumerique;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsAuditConformiteContratNumeriqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsAuditConformiteContratNumeriqueApplication.class, args);
+	}
+
+}
